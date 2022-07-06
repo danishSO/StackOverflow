2b9e8bba63203303e338813defe727e5f12d7469
Danish <danish.work.id@gmail.com>
2022-07-06 17:36:55 +0200

    commit for https://stackoverflow.com/questions/72857011/sql-case-when-lineid-01-then-newid-else-id and https://stackoverflow.com/questions/72872600/sql-i-am-doing-a-join-amongst-three-tables-but-i-want-to-sort-one-column-by-mo

---
+++ SOSamples/StackOverflow/ColumnCompare.sql
@@ -0,0 +1,11 @@
+﻿-- Compare dates in 3 columns and use the most recent one in one date column. did not create table for this one
+SELECT TOP 1	Machines.[Id],
+				Machines.[Location],
+				(	SELECT MAX(LastUpdateDate) 
+					FROM (VALUES (Machines.[Date]), (Systems.[Date]), (Laptops.[Date])) AS UpdateDate(LastUpdateDate)) AS LastUpdateDate
+FROM			dbo.ISD_machines Machines 
+	INNER JOIN	dbo.ISD_systems Systems
+					ON a.HOSTNAME = b.SYSTEM_NAME
+	INNER JOIN	dbo.ISD_laptops Laptops
+					ON a.HOSTNAME = c.[NAME] 
+ORDER BY LastUpdateDate desc
---
+++ SOSamples/StackOverflow/PartitionOver.sql
@@ -0,0 +1,30 @@
+﻿-- How many rows per line id exists in the result? Sequences them per line id
+
+USE [StackOverflow]
+GO
+
+/****** Object:  Table [dbo].[RunningCount]    Script Date: 7/6/2022 5:33:57 PM ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[RunningCount]') AND type in (N'U'))
+DROP TABLE [dbo].[RunningCount]
+GO
+
+/****** Object:  Table [dbo].[RunningCount]    Script Date: 7/6/2022 5:33:57 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[RunningCount](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[LineId] [int] NULL,
+	[Price] [int] NULL
+) ON [PRIMARY]
+GO
+
+
+
+SELECT ROW_NUMBER() over(partition by [LineId] order by [LineId]) as DocId,
+      [LineId],
+      [Price]
+  FROM [StackOverflow].[dbo].[RunningCount] order by [LineId]
