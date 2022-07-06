6dd44689b088f5772b7c6d224c8186be5848bd4e
Danish Gite <danish.gite@3ds.com>
2022-07-06 13:18:24 +0200

    Contract class structure for question https://stackoverflow.com/questions/72742959/combine-two-classes-from-a-table-per-hierarchy-inheritance-structure-in-entity-f

---
+++ SOSamples/SOSamples/ContractStructure.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOSamples
+{
+    public class Contract
+    {
+        public int ContractId
+        {
+            get;
+            set;
+        }
+        public DateTime StartDate
+        {
+            get;
+            set;
+        }
+        public int Months
+        {
+            get;
+            set;
+        }
+        public decimal Charge
+        {
+            get;
+            set;
+        }
+
+        public bool IsMultiContract
+        {
+            get
+            {
+                return SubContracts.GroupBy(x => x.ContractType).Count() > 0;
+            }
+        }
+
+        /// <summary>
+        /// Optional unique check for subcontract types may be added
+        /// </summary>
+        public List<SubContract> SubContracts
+        {
+            get;
+            set;
+        }
+    }
+
+    public enum ContractType
+    {
+        Broadband,
+        Mobile,
+        TV
+    }
+
+
+    public class SubContract
+    {
+        public ContractType ContractType
+        {
+            get;
+            set;
+        }
+    }
+
+    public class MobileContract : SubContract
+    {
+        public string MobileNumber
+        {
+            get;
+            set;
+        }
+    }
+
+    public class TvContract : SubContract
+    {
+        public PackageType PackageType
+        {
+            get;
+            set;
+        }
+    }
+
+    public class BroadBandContract : SubContract
+    {
+        public int DownloadSpeed
+        {
+            get;
+            set;
+        }
+    }
+
+    public enum PackageType
+    {
+        S,
+        M,
+        L,
+        XL
+    }
+}
