b2fcc7eab4ca9e1ee865e6a1e4b3dfedb8793d58
Danish <danish.work.id@gmail.com>
2022-08-29 13:33:20 +0200

    Removed unused files

--- SOSamples/SAMLIDP/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SAMLIDP.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- SOSamples/SAMLIDP/Models/LoginViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SAMLIDP.Models
-{
-    public class LoginViewModel
-    {
-        public string? UserName { get; set; }
-
-        public string? Password { get; set; }
-    }
-}
