28a640ed80647063ddf09686829e26f0c593998c
Danish <danish.work.id@gmail.com>
2022-08-29 13:32:08 +0200

    Removed MVC application

---
+++ SOSamples/SAMLIDP/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SAMLIDP.Models;
+using System.Diagnostics;
+using System.DirectoryServices;
+
+
+namespace SAMLIDP.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel loginModel)
+        {
+            if (loginModel != null)
+            {
+                // Verify username and password
+
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult LoginUsingDSONE()
+        {
+            using (DirectoryEntry de = new DirectoryEntry("LDAP://MyDomainController"))
+            {
+                using (DirectorySearcher adSearch = new DirectorySearcher(de))
+                {
+                    adSearch.Filter = "";
+                    SearchResult adSearchResult = adSearch.FindOne();
+                }
+            }
+
+            return Redirect("http://www.google.com");
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SOSamples/SAMLIDP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SAMLIDP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
